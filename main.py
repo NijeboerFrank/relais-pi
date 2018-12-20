a1fd7b9c228f1b092eeb2fcfe8ea09eeea45cc6c
NijeboerFrank <frank.nijeboer@extendas.com>
2018-12-20 13:21:18 +0100

    made function of turning relay

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ GPIO.setmode(GPIO.BCM)
 
 # Init list with pin numbers
 
-pinList = [2, 3, 4, 17]
+pinList = [17, 4, 3, 2]
 
 # Loop through pins and set mode and state to 'high'
 print("setting pins")
@@ -27,38 +27,39 @@ def basic_test():
 
 @app.route("/4")
 def run_4():
-    GPIO.output(pinList[0], GPIO.LOW)
-    time.sleep(sleep_time)
-    GPIO.output(pinList[0], GPIO.HIGH)
+    switch_relay(number=4, slp_time=1)
     return "Switched light 4!"
 
 
 @app.route("/3")
 def run_3():
-    GPIO.output(pinList[1], GPIO.LOW)
-    time.sleep(sleep_time)
-    GPIO.output(pinList[1], GPIO.HIGH)
+    switch_relay(number=3, slp_time=1)
     return "Switched light 3!"
 
 
 @app.route("/2")
 def run_2():
-    GPIO.output(pinList[2], GPIO.LOW)
-    time.sleep(sleep_time)
-    GPIO.output(pinList[2], GPIO.HIGH)
+    switch_relay(number=2, slp_time=1)
     return "Switched light 2!"
 
 
 @app.route("/1")
 def run_1():
-    GPIO.output(pinList[3], GPIO.LOW)
-    time.sleep(sleep_time)
-    GPIO.output(pinList[3], GPIO.HIGH)
+    switch_relay(number=1, slp_time=1)
     return "Switched light 1!"
 
 
+def switch_relay(number, slp_time):
+    slp = slp_time
+    GPIO.output(pinList[number - 1], GPIO.LOW)
+    time.sleep(slp)
+    GPIO.output(pinList[number - 1], GPIO.HIGH)
+
 @atexit.register
 def exit_app():
+    """
+    This function is called when the server is stopped. This is important for a clean exit of the GPIO pins.
+    """
     print("  Exiting Server...")
     GPIO.cleanup()
 
