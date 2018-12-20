2bcbed239264c3179e67e0789e20322abe52aed1
NijeboerFrank <frank.nijeboer@extendas.com>
2018-12-20 15:14:16 +0100

    made timeout

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ GPIO.setmode(GPIO.BCM)
 
 # Init list with pin numbers
 # Order depending on how the GPIO pins are hooked up, in this case GPIO 17 controls relay 1
-pinList = [17, 4, 3, 2]
+pin_list = [17, 4, 3, 2]
+input_pin = 12
 
 # Loop through pins and set mode and state to 'high'
 print("setting pins")
-for i in pinList:
+for i in pin_list:
     GPIO.setup(i, GPIO.OUT)
     GPIO.output(i, GPIO.HIGH)
 
@@ -29,8 +30,12 @@ def basic_test():
 
 @app.route("/4")
 def run_4():
-    switch_relay(number=4, slp_time=1)
-    return "Switched light 4!"
+    ret = GPIO.wait_for_edge(input_pin, GPIO.RISING, timeout=5000)
+    if ret is None:
+        return "Timeout occurred"
+    else:
+        switch_relay(number=4, slp_time=1)
+        return "Switched light 4!"
 
 
 @app.route("/3")
@@ -52,10 +57,15 @@ def run_1():
 
 
 def switch_relay(number, slp_time):
+    """
+    Function that switches a relay on.
+    :param number: Which relay should be switched on.
+    :param slp_time: How long should the relay be switched on. 
+    """
     slp = slp_time
-    GPIO.output(pinList[number - 1], GPIO.LOW)
+    GPIO.output(pin_list[number - 1], GPIO.LOW)
     time.sleep(slp)
-    GPIO.output(pinList[number - 1], GPIO.HIGH)
+    GPIO.output(pin_list[number - 1], GPIO.HIGH)
 
 
 @atexit.register
@@ -68,9 +78,14 @@ def exit_app():
 
 
 def callback_input(pin):
+    """
+    Function that keeps a relay powered on when a button is pressed. (In this case when GPIO 12 reads input)
+    :param pin: Pin number of the GPIO pin.
+    """
     while GPIO.input(pin) == 1:
-        GPIO.output(pinList[1], GPIO.LOW)
-    GPIO.output(pinList[1], GPIO.HIGH)
+        GPIO.output(pin_list[1], GPIO.LOW)
+    GPIO.output(pin_list[1], GPIO.HIGH)
 
 
+# Add callback for button press
 GPIO.add_event_detect(12, GPIO.RISING, callback=callback_input)  # add rising edge detection on a channel
