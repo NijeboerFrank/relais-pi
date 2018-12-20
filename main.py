57bc05f48a7bd1c98691abeef3f8fa65901ba93a
NijeboerFrank <frank.nijeboer@extendas.com>
2018-12-20 11:59:36 +0000

    some changes from the pi

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 import RPi.GPIO as GPIO
 import time
 
-sleep_time = 2
+sleep_time = 1
 
 app = Flask(__name__)
 
@@ -14,7 +14,7 @@ try:
     pinList = [2, 3, 4, 17]
 
     # Loop through pins and set mode and state to 'high'
-
+    print("setting pins")
     for i in pinList:
         GPIO.setup(i, GPIO.OUT)
         GPIO.output(i, GPIO.HIGH)
@@ -25,37 +25,37 @@ try:
         return "Server works!"
 
 
-    @app.route("/1")
+    @app.route("/4")
     def run_4():
         GPIO.output(pinList[0], GPIO.LOW)
         time.sleep(sleep_time)
         GPIO.output(pinList[0], GPIO.HIGH)
-        return "Switched light 1!"
+        return "Switched light 4!"
 
 
-    @app.route("/2")
+    @app.route("/3")
     def run_3():
         GPIO.output(pinList[1], GPIO.LOW)
         time.sleep(sleep_time)
         GPIO.output(pinList[1], GPIO.HIGH)
-        return "Switched light 2!"
+        return "Switched light 3!"
 
 
-    @app.route("/3")
+    @app.route("/2")
     def run_2():
         GPIO.output(pinList[2], GPIO.LOW)
         time.sleep(sleep_time)
         GPIO.output(pinList[2], GPIO.HIGH)
-        return "Switched light 3!"
+        return "Switched light 2!"
 
 
-    @app.route("/4")
+    @app.route("/1")
     def run_1():
         GPIO.output(pinList[3], GPIO.LOW)
         time.sleep(sleep_time)
         GPIO.output(pinList[3], GPIO.HIGH)
-        return "Switched light 4!"
+        return "Switched light 1!"
 
-except KeyboardInterrupt:
+except:
     print(" Exiting server")
     GPIO.cleanup()
